5033293ce74cb8508952a3ed788f884764cb7b25
pavloperforator <ytuber9228@gmail.com>
2026-08-10 18:13:48 +0300

    Оновив усі сторінки та файли

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let meccha = document.querySelector('#meccha');
+    let dota2 = document.querySelector('#dota2');
+    
+
+    let buttons = document.querySelectorAll('.slider_arrow');
+
+    let slides = [meccha, dota2, ];
+
+    let current = 0;
+
+    
+
+    function showSlide() {
+
+        slides.forEach(function(slide) {
+            slide.classList.add('hide');
+        });
+
+        slides[current].classList.remove('hide');
+    }
+
+    setInterval(function() {
+                 current++;
+
+        if (current >= slides.length) {
+            current = 0;
+        }          
+
+        showSlide();
+    }, 10000)
+
+
+    buttons[1].onclick = function() {
+
+        current++;
+
+        if (current >= slides.length) {
+            current = 0;
+        }
+
+        showSlide();
+    };
+
+
+    buttons[0].onclick = function() {
+
+        current--;
+
+        if (current < 0) {
+            current = slides.length - 1;
+        }
+
+        showSlide();
+    };
+
+
+    showSlide();
+
+
+});
